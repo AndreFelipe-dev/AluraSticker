1e7bc1666d76832ede79d35847380f9af96b2512
AndreFelipe-dev <felipeandre.dev08@gmail.com>
2023-03-30 00:27:41 -0300

    geradora de figurinhas

--- Program.java
+++ Program.java
@@ -1,6 +1,8 @@
 package principal;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -14,7 +16,7 @@ public class Program {
 
 		// fazer uma conexão HTTP e buscar os top 250 filmes
 		// String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-		
+
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 		URI endereco = URI.create(url);
 		var client = HttpClient.newHttpClient();
@@ -23,16 +25,24 @@ public class Program {
 		String body = response.body();
 
 		// extrair só os dados que interessam (titulo, poster, classificação)
-		
+
 		var parse = new JsonParse();
 		List<Map<String, String>> listaDeFilmes = parse.parse(body);
 
 		// exibir e manipular os dados
-		
+
 		for (Map<String, String> filme : listaDeFilmes) {
+			
+			String urlImagem= filme.get("Image");
+			String titulo= filme.get("title");
+			InputStream inputStream= new  URL(urlImagem).openStream();
+			
+			String nomeArquivo= titulo + ".png";
+			
+			var geradora = new geradoraDeFigurinhas();
+			geradora.cria(inputStream,nomeArquivo);
+			
 			System.out.println(filme.get("title"));
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));
 			System.out.println();
 		}
 	}
